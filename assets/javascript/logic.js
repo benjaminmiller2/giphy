784f3fcf3308c2d287a12149809f902da8edf5cf
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-03-27 14:00:19 -0500

    c

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,19 +1,28 @@
 
 let search = "futurama";
-let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Xf0hnGeWnHIBhnq3lXRZSAj470HT1XbM&q=" + search + "&limit=5&offset=0&rating=PG&lang=en"
+let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Xf0hnGeWnHIBhnq3lXRZSAj470HT1XbM&q=" + search + "&limit=10&offset=0&rating=PG&lang=en"
+
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
-    console.log(response);
 
-    let gifImage = $("<img>");
-   
+    for(var i = 0; i<10; i++){
+let results = response.data;
+let imgURL = results[i].images.downsized_still.url;//still imag
 
-    $(".js-gif").append(gifImage);
-    
-    gifImage.attr("src", response.data[0].images.downsized.url);
+let img = $("<img>");
+        img.attr("src", imgURL);
 
+let gifBox = $("<div>");
+
+gifBox.append(img);
+
+$(".js-gif-box").append(gifBox);
+
+
+};
 
 });
